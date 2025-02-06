312872518f17530473d176fa0170d42a4e297f76
Wellington <w9llington@gmail.com>
2025-02-06 14:42:36 -0300

    imports fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,9 +9,11 @@ import {
   StartCountdownButton,
   StopCountdownButton,
 } from './styles'
-import { NewCycleForm } from './components/NewCycleForm'
-import { Countdown } from './components/Countdown'
+
+
 import { CyclesContext } from '../../contexts/CyclesContext'
+import { NewCycleForm } from './Components/NewCycleForm'
+import { Countdown } from './Components/Countdown'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
